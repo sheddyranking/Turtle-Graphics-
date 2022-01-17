ada707f3a89d90f021c7409562fdc2c3928b0755
sheddyranking <shedyisaac@gmail.com>
2022-01-17 23:40:19 +0100

    added another square on the same shit

--- square.py
+++ square.py
@@ -7,6 +7,7 @@ square = turtle.Turtle()
 square.color("blue", "cyan")
 
 #wrap your design in begin.fill() and end.fill() ,function to cloud the color
+#you cound wrap each square  with begin() and end() fill or wrap them in one begin() and end() fill
 square.begin_fill()
 square.forward(100)
 square.left(90)
@@ -15,8 +16,22 @@ square.left(90)
 square.forward(100)
 square.left(90)
 square.forward(100)
-square.end_fill()
+#square.end_fill()
 
+#the penup() and pendown() function will  help break out of the first square.
+square.penup()
+square.forward(100)
+square.pendown()
+
+#square.begin_fill()
+square.forward(100)
+square.left(90)
+square.forward(100)
+square.left(90)
+square.forward(100)
+square.left(90)
+square.forward(100)
+square.end_fill()
 
 
 
