0c9a0c38a2a94b3019712e2340db4cb3f9307569
sheddyranking <shedyisaac@gmail.com>
2022-01-17 23:26:22 +0100

    updated readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ An overview of Turtle Graphics with python
 
 NOTE:
 when writting a program that involes 'turtle', don't name your file turtle.py, because when importing it will import the file instead of the packages.
+
+the begin.fill() and end.fill() functions are used to cloud the are with the color
--- square.py
+++ square.py
@@ -2,7 +2,8 @@ import turtle
 
 square = turtle.Turtle()
 
-#add color, you could use hash codes
+#add color, you could use hash codes, 
+#the first color cloud the area, and the second color draws a margin around.
 square.color("blue", "cyan")
 
 #wrap your design in begin.fill() and end.fill() ,function to cloud the color
