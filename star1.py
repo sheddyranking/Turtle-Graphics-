a99af74acb39c3ef05437f12032e8fc717feeb81
sheddyranking <shedyisaac@gmail.com>
2022-01-18 21:24:15 +0100

    added the star.py

--- star1.py
+++ star1.py
@@ -1,13 +1,27 @@
 import turtle
 
 star = turtle.Turtle()
-star.getscreen().bgcolor("#994444")
+star.getscreen().bgcolor("#994444") #changes background color 
+star.speed(20) #speed up the drawing 
 
 
-for i in range(5):
-    star.forward(100)
-    star.left(216)
- 
+star.penup()
+star.goto((-200, 100))
+star.pendown()
+
+#the functions that draws the star shape
+def sheddy(draw, size):
+    if size <= 10: # the recursive statement
+        return
+    else:
+        star.begin_fill()
+        for i in range(5):
+            draw.forward(size)
+            sheddy(draw, size/3)
+            draw.left(216)
+        star.end_fill()
+
+sheddy(star, 360) #pass in the const variable and the value of the size 
 
 
 
