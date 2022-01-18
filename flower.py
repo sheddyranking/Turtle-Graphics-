1bb17ffe0f2f963f11d244824517fb7699751fac
sheddyranking <shedyisaac@gmail.com>
2022-01-18 21:26:58 +0100

    updated readme

--- flower.py
+++ flower.py
@@ -1,12 +1,12 @@
 import turtle
 
 flower = turtle.Turtle()
-flower.color("red", "yellow")
-flower.speed(10)
+flower.color("red", "yellow") #red for the edge and yellow for the fill.
+flower.speed(10) #speed up thee drawing 
 
 
 flower.begin_fill()
-for i in range(100):
+for i in range(100): # the statement that draws the flower shape
     flower.forward(300)
     flower.left(170)
 
